578b9c5054965eaaa817700eac3f4199714f56a3
RUTIKVILAYATE <rutik.vilayate.ai@gmail.com>
2024-12-05 00:06:22 +0530

    installation guide

---
+++ 02_installation.py
@@ -0,0 +1,25 @@
+# Install VSCode
+
+# Install Java (openjdk) -> go to React Native website, React Native CLI Quickstart -> see command to install java 
+# go to command line in local machine -> paste the command -> choco install -y nodejs-lts microsoft-openjdk11
+# check if it got installed or not using java --version 
+
+# Install Android Studio -> and check -> adb --version (which is useful to build/run react native application on mobile phone)
+# Why to install Android Studio --> to use Android SDK Platform-Tools
+
+
+
+
+# Explain
+# Install NodeJs --> Go to NodeJs then install it's current or any version you want to install
+# after installation check if its installed or not using node -v command in cmd and check npm -v
+
+
+# Install Jdk means java --> go to OpenJDK or openlogics to install it
+# set the path 
+
+# Install Android Studio
+
+
+
+# Basically, See the video to install all of the things
